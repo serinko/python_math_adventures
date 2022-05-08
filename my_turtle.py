4579cafd13895788eec206848fa8e63cb3fe9064
serinko <johanesxsalmon@riseup.net>
2022-05-08 10:05:15 +0200

    turtle 60 squares

--- my_turtle.py
+++ my_turtle.py
@@ -5,11 +5,14 @@ from turtle import *
 # forward(150)
 
 shape('turtle')
+def square():
+    for i in range(4):
+        forward(100)
+        right(90)
 
-for i in range(4):
-    forward(100)
-    right(90)
-
+for i in range(60):
+    square()
+    right(5)
 
 #
 # # from turtle import Turtle, Screen
