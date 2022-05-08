d18d6d2f6d7c9caa2ffc71ada24c73475dd61f9c
serinko <johanesxsalmon@riseup.net>
2022-05-08 09:53:50 +0200

    added my screen

--- my_turtle.py
+++ my_turtle.py
@@ -2,3 +2,11 @@ from turtle import *
 
 forward(100)
 shape('turtle')
+#
+# # from turtle import Turtle, Screen
+# jabba = Turtle()
+# print(jabba)
+
+my_screen = Screen()
+# print(my_screen.canvheight)
+my_screen.exitonclick()
