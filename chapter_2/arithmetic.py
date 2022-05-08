97186bf1457269dce1843234d8fb10098d4e15ce
serinko <johanesxsalmon@riseup.net>
2022-05-08 16:16:33 +0200

    mySum()

--- chapter_2/arithmetic.py
+++ chapter_2/arithmetic.py
@@ -5,4 +5,12 @@ running_sum = 0
 for i in range(10):
     running_sum += 3
 
-print(running_sum)
+
+
+def mySum(num):
+    running_sum = 0
+    for i in range(1,num+1):
+        running_sum += i
+    return running_sum
+
+print(mySum(50))
