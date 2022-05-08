887f987c18428a841d0088af1f411573458843ce
serinko <johanesxsalmon@riseup.net>
2022-05-08 11:12:05 +0200

    turtle summary

---
+++ chapter_1/README.md
@@ -0,0 +1,3 @@
+# Turtle Documentation
+
+[https://docs.python.org/3/library/turtle.html](docs.python.org/3/library/turtle.html)
---
+++ chapter_1/my_turtle.py
@@ -0,0 +1,40 @@
+from turtle import *
+
+# forward(100)
+# right(45)
+# forward(150)
+
+shape('turtle')
+def square(sidelength=100):
+    for i in range(4):
+        forward(sidelength)
+        right(90)
+
+def triangle(sidelength=100):
+    for i in range(3):
+        forward(sidelength)
+        right(120)
+
+
+
+#
+# # from turtle import Turtle, Screen
+# jabba = Turtle()
+# print(jabba)
+
+def polygon(sides=6, sidelength=100):
+    for i in range(sides):
+        forward(sidelength)
+        turn_angle = 360/sides
+        right(int(turn_angle))
+
+length = 5
+for i in range(60):
+    square(length)
+    length += 5
+    right(5)
+
+my_screen = Screen()
+# print(my_screen.canvheight)
+my_screen.exitonclick()
+
