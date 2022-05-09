ed19de1cdd32c15f304ec7ec1d2549cd3a8aedd8
serinko <johanesxsalmon@riseup.net>
2022-05-09 10:02:28 +0200

    randomly moving turtle

---
+++ chapter_3/wander.py
@@ -0,0 +1,10 @@
+from turtle import *
+from random import randrange
+
+def wander():
+    while True:
+        fd(3)
+        if xcor() >= 200 or xcor() <= -200 or ycor() <= -200 or ycor() >= 200:
+            lt(randrange(90,180))
+
+wander()
