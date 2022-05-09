8198ae161f457a37b61e21a8b751f3116c07e21b
serinko <johanesxsalmon@riseup.net>
2022-05-09 09:36:31 +0200

    factors

---
+++ chapter_3/factors.py
@@ -0,0 +1,14 @@
+# TODO
+# 1. Define the factors Function, which takes a number as an argument
+# 2. Create an empty factors list to fill factors.
+# 3. Loop over all the numbers from 1 to the given number
+# 4. If any of these numbers divides evenly, add it to the factors list.
+# 5. Return the list of factors at the end.
+
+def factors(num):
+    factors = []
+    for i in range(1,num):
+        if num % i == 0:
+            factors.append(i)
+    return factors
+
