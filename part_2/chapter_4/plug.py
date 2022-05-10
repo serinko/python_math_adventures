d86e79991266a253d3a91c249e8ebc7d4bc37e6d
serinko <johanesxsalmon@riseup.net>
2022-05-10 09:20:13 +0200

    edits

--- part_2/chapter_4/plug.py
+++ part_2/chapter_4/plug.py
@@ -3,7 +3,7 @@
 def plug():
     x = -100 # start at -100
     while x < 100: # go up to 100
-        if 2*x + 5 == 13: # if it makes the equation true
+        if 2 * x + 5 == 13: # if it makes the equation true
             print("x =",x) # print it out
         x += 1 # make x go up b 1 to test the next number
 
